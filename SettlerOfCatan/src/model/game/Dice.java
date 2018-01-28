b925b3dedf3230df740756526cb4da878f44a7e5
Alexander <hofmeisteralexander@googlemail.com>
2018-01-28 18:58:24 +0100

    Roll the dice in the constuctor, so we can remove duplicate calls

--- SettlerOfCatan/src/model/game/Dice.java
+++ SettlerOfCatan/src/model/game/Dice.java
@@ -14,26 +14,25 @@ public class Dice extends StackPane {
   private static final int MIN_NUMBER = 1;
   private static final int MAX_NUMBER = 6;
 
-  @Getter
-  private int number;
+  private static int getRandomDiceNumber() {
+    return new Random().ints(MIN_NUMBER, MAX_NUMBER + 1).findFirst().getAsInt();
+  }
 
   @Getter
+  private int number;
+  @Getter
   private final Rectangle diceShape = new Rectangle(50, 50);
+
   private final Label numberLabel;
 
   public Dice() {
-    this.number = getRandomDiceNumber();
     this.diceShape.setFill(Color.WHITE);
     this.diceShape.setStroke(Color.BLACK);
     this.diceShape.setStrokeWidth(2);
+    this.numberLabel = new Label();
     getChildren().add(this.diceShape);
-    this.numberLabel = new Label(String.valueOf(this.number));
     getChildren().add(this.numberLabel);
-  }
-
-  private int getRandomDiceNumber() {
-    final Random random = new Random();
-    return random.ints(MIN_NUMBER, MAX_NUMBER + 1).findFirst().getAsInt();
+    roll();
   }
 
   public void roll() {
